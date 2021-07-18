22ddd30b282f8a89c55ef33d29d907fe20b8dd8a
Rutik K Patel <72887393+rutikkpatel@users.noreply.github.com>
2021-07-19 02:10:02 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// *** Preloader script ***
+function mypreloadfun(){
+    document.getElementById("preloader").style.display = "none";
+}
+
+// *** BACK TO TOP script ***
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 400);
+            return false;
+        });
+    });
